100caf0c6277af6fc7fbe366fa8262b5b7389424
OmarKh006 <khairyomar006@gmail.com>
2026-07-14 01:31:28 +0300

    challenge 1

---
+++ dist/challenge1.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function calculate(operation, ...nums) {
+    if (nums.length < 2) {
+        return "Error: At least two numbers are required";
+    }
+    if (operation === "add")
+        return nums.reduce((acc, num) => acc + num);
+    else if (operation === "subtract")
+        return nums.reduce((acc, num) => acc - num);
+    else if (operation === "multiply")
+        return nums.reduce((acc, num) => acc * num);
+    else if (operation === "divide") {
+        return nums.reduce((acc, num) => {
+            if (num === 0) {
+                throw new Error("Error: Division by zero");
+            }
+            return acc / num;
+        });
+    }
+    else {
+        return "Error: Invalid operation";
+    }
+}
+console.log(calculate("add", 5, 3, 2));
+console.log(calculate("subtract", 10, 4, 4));
+console.log(calculate("multiply", 2, 2, 5));
+console.log(calculate("divide", 15, 3, 5));
+//# sourceMappingURL=challenge1.js.map
