1a8347ba6bb4f966b60693748419e83a94baa4d2
OmarKh006 <khairyomar006@gmail.com>
2026-07-13 21:26:55 +0300

    working with tsconfig file

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function greet(name) {
     return `Hello ${name}, welcome to typescript`;
 }
 const greeting = greet("omar");
 console.log(greeting);
+//# sourceMappingURL=index.js.map
